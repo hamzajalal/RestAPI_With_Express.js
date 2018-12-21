0991bc3b4cebfcc114ec7331af16b392eaea6f5f
hamzajalal <hamza.jalal2@myy.haaga-helia.fi>
2018-12-21 14:37:10 +0200

    Added the text analyze logic to the api template

--- index.js
+++ index.js
@@ -1,53 +1,67 @@
-const express = require('express');
 const Joi = require('joi');
+const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
-app.use(express.json());
- 
-const realData = [];
+app.use(bodyParser.json());
 
-app.get('/api/analyze', (req, res) => {
-    //Getting data from Database but in this case, only focusing on endpoint
-  res.send(realData)
-});
+const characterList = (text) => {
+    let characterArray = text.toLowerCase().split("").filter(e => /[a-z]/.test(e))
+    characterArray.sort();
 
+    const characters = [];
+    let counter = 0;
+    for (let i = 0; i < characterArray.length; i++) {
+        let character = characterArray[i]
+
+        counter++
+        let nextCharacter = characterArray[i + 1]
+
+        if (character.localeCompare(nextCharacter)) {
+            characters.push({ [character]: counter })
+            counter = 0;
+        }
+    }
+    return characters;
+}
 
 app.post('/api/analyze', (req, res) => {
-    const { error } = validateDataItem(req.body); //result.error
+    const { error } = validateCharacterListItem(req.body); //result.error
     if (error) return res.status(400).send(error.details[0].message);
 
+    const { text } = req.body;
 
-    const dataItem = {
-        //id: realData.length + 1,
-        content: req.body.content
+    const textWithoutSpaces = text.replace(/\ /g, "")
+    const textLength = {
+        withSpaces: text.length,
+        withoutSpaces: textWithoutSpaces.length
     };
-    realData.push(dataItem);
+
+    let splitText = text.split(" ").filter(String);
+    const countWord = splitText.length;
+
+    const characterCount = characterList(text);
+
+    const result = {
+        textLength: textLength,
+        countWord: countWord,
+        characterCount: characterCount
+    };
     
-  res.send( dataItem);
+  res.send( result );
 });
 
 
-// app.put('/api/analyze', (req, res) => {
-//     const dataItem = realData.find(dataItem => dataItem.id === parseInt(req.params.id));
-//     if (!dataItem) return res.status(404).send('The Item does not Exist!');
-    
-//     //const result = validateCourse(req.body);
-//     const { error } = validateDataItem(req.body); //result.error
-//     if (error) return res.status(400).send(error.details[0].message);
-
-//     dataItem.name = req.body.name;
-//     res.send(dataItem);
-// });
-
-function validateDataItem(dataItem) {
+// Joi validation was not needed for this task but I have just added it for validation purpose of my own practice.
+function validateCharacterListItem(text) {
     const schema = {
-        content: Joi.string().min(3).required()
+        text: Joi.string().min(3).max(500).required()
     };
 
-    return Joi.validate(dataItem, schema);
+    return Joi.validate(text, schema);
     //console.log(result);
 }
- 
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log( `Listening to port ${port}...` ));
+
+const port = process.env.PORT || 3000; //export PORT=8585
+app.listen(port, () => console.log( `Listening to Port ${port}...` ));
